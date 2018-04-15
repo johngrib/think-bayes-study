7d02649f4e95bb247616cd58202652f76e6c2b13
이종립 <johngrib@woowahan.com>
2018-04-15 14:31:42 +0900

    train problem

---
+++ code/train.js
@@ -0,0 +1,61 @@
+// hypos: 가설의 배열
+// 가설의 배열을 돌며 같은 경우의 수 1을 부여한다
+function init(hypos) {
+    const dict = {};
+    hypos.forEach((h) => {
+        dict[h] = 1;
+    });
+    return dict;
+}
+
+// 모든 가설을 돌며 mix의 data에 해당하는 값을 곱해 업데이트한다
+function update(dict, data) {
+
+    Object.keys(dict).forEach((hypo) => {
+        dict[hypo] = dict[hypo] * likelihood(hypo, data);
+    });
+
+    return normalize(dict);
+}
+
+// p(D | H_n)
+function likelihood(hypo, data) {
+    if (hypo < data) {
+        return 0;
+    }
+    return 1 / hypo;
+}
+
+// 모든 가설의 확률의 비율을 유지하며, 총합이 1이 되도록 정규화한다
+function normalize(dict) {
+    const values = Object.values(dict);
+    const sum = values.reduce((a, b) => a + b);
+    const result = {};
+    Object.keys(dict).forEach((key) => {
+        result[key] = dict[key] / sum;
+    });
+    return result;
+}
+
+function range(start, size) {
+    return [...Array(size).keys()].map((n) => n + start);
+}
+
+function main() {
+    const hypos = range(1, 1000);
+    const pmf = init(hypos);
+    const result = update(pmf, 60);
+
+    Object.keys(result).forEach((k) => {
+        console.log(k + '\t' + result[k])
+    });
+
+    const result2 = normalize(result);
+    const rs = Object.keys(result2).reduce((a, b) => {
+        return a + (b * result2[b])
+    }, 0);
+    console.log(rs);
+}
+
+
+main();
